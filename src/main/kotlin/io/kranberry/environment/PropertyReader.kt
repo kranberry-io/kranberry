3fa34e2be7c3dc7ac31f3d9adb443678cd0d0e1d
Ana Ludmila <kn.ludi@gmail.com>
2021-03-08 22:36:39 -0300

    #2 @knludi - [WIP] Include kranberry.properties file

--- src/main/kotlin/io/kranberry/environment/PropertyReader.kt
+++ src/main/kotlin/io/kranberry/environment/PropertyReader.kt
@@ -1,19 +1,37 @@
 package io.kranberry.environment
 
+import androidx.test.platform.app.InstrumentationRegistry
 import java.io.FileInputStream
 import java.lang.System.getProperty
+import java.nio.file.Paths
 import java.util.*
 
 class PropertyReader {
 
     fun getProperty() {
-        val properties = Properties()
+/*        val properties = Properties()
 
-        val propertiesFile = getProperty("user.dir") + "kranberry.properties"
+        val propertiesFile = "kranberry.properties"
+
 
         val inputStream = FileInputStream(propertiesFile)
-        properties.load(inputStream)
+        properties.load(inputStream)*/
 
-        properties.forEach{(k, v) -> println("key = $k, value = $v")}
+        //val properties = Properties()
+        val properties = readFileAsText("kranberry.properties")
+        println(properties)
+
+        //properties.forEach{(k, v) -> println("key = $k, value = $v")}
+    }
+
+    private fun readFileAsText(fileName: String): String {
+        return InstrumentationRegistry
+            .getInstrumentation()
+            .context
+            .resources
+            .assets
+            .open(fileName)
+            .bufferedReader()
+            .readText()
     }
 }
