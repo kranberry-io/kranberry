6e3ddaf7619fd591b925957ebb9945f74dc8615a
Ana Ludmila <kn.ludi@gmail.com>
2021-07-31 16:38:28 -0300

    #6 @knludi - Extracting test execution behaviors

--- src/main/kotlin/io/kranberry/environment/TestHandler.kt
+++ src/main/kotlin/io/kranberry/environment/TestHandler.kt
@@ -1,19 +1,43 @@
 package io.kranberry.environment
 
+import androidx.test.uiautomator.UiDevice
+import kotlin.math.round
+
 object TestHandler {
 
+    lateinit var device: UiDevice
     lateinit var currentTestMethodName: String
     lateinit var currentTestClassName: String
     var testClassName = ""
+    var currentTestCaseId = ""
 
-    var failedTestCount: Double = 0.0
-    var passedTestCount: Double = 0.0
-    var totalTestCount: Double = 0.0
+    var currentTestResult = "PASSED"
+
+    var failedTestCount: Int = 0
+    var passedTestCount: Int = 0
+    var totalTestCount: Int = 0
+    var passedRate: Double = 0.0
+    var failedRate: Double = 0.0
 
     var reportHasHeader: Boolean = false
 
 
     fun testClassName(testClassName: String) {
         this.testClassName = testClassName
+    }
+
+    fun updateSummary() {
+        totalTestCount++
+
+        passedRate = round((passedTestCount.toDouble() / totalTestCount) * 100)
+        failedRate = round((failedTestCount.toDouble() / totalTestCount) * 100)
+    }
+
+    fun testCaseId(testCaseId: String) {
+        currentTestCaseId = testCaseId
+    }
+
+    fun testResult(testResult: String) {
+        currentTestResult = testResult
     }
 }
