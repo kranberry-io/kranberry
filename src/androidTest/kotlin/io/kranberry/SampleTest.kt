f0cbf32cf0a681c5a65becb0aba04bd184e2d6f9
Ana Ludmila <kn.ludi@gmail.com>
2021-07-31 16:34:50 -0300

    #11 @knludi - Replacing BaseTest with KranberryRules

--- src/androidTest/kotlin/io/kranberry/SampleTest.kt
+++ src/androidTest/kotlin/io/kranberry/SampleTest.kt
@@ -2,14 +2,19 @@ package io.kranberry
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SdkSuppress
-import io.kranberry.test.BaseTest
+import io.kranberry.environment.TestHandler.device
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 @SdkSuppress(minSdkVersion = 21)
 
-class SampleTest : BaseTest() {
+class SampleTest {
+
+    @Rule
+    @JvmField
+    val testRule = KranberryRules()
 
     @Test
     fun openGoogleChrome() {
