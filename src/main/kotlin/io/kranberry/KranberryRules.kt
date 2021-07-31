7d276d3e7085d331aa956a5288138f8d499e8604
Ana Ludmila <kn.ludi@gmail.com>
2021-07-31 16:39:58 -0300

    #6 @knludi - Extracting behaviors of test execution and logs

--- src/main/kotlin/io/kranberry/KranberryRules.kt
+++ src/main/kotlin/io/kranberry/KranberryRules.kt
@@ -1,71 +1,71 @@
 package io.kranberry
 
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.uiautomator.UiDevice
 import io.kranberry.ScreenshotHandler.getExecutionScreenshotPath
 import io.kranberry.ScreenshotHandler.getTestResult
 import io.kranberry.ScreenshotHandler.setScreenshotsName
 import io.kranberry.ScreenshotHandler.takeScreenshot
-import io.kranberry.ScreenshotHandler.testResult
 import io.kranberry.environment.DeviceHandler.testEnvironmentProperties
 import io.kranberry.environment.TestHandler.currentTestClassName
 import io.kranberry.environment.TestHandler.currentTestMethodName
+import io.kranberry.environment.TestHandler.device
 import io.kranberry.environment.TestHandler.failedTestCount
 import io.kranberry.environment.TestHandler.passedTestCount
 import io.kranberry.environment.TestHandler.reportHasHeader
 import io.kranberry.environment.TestHandler.testClassName
-import io.kranberry.environment.TestHandler.totalTestCount
+import io.kranberry.environment.TestHandler.testResult
+import io.kranberry.environment.TestHandler.updateSummary
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 import java.io.File
-import kotlin.math.round
 
-open class TestRules() : TestWatcher() {
+open class KranberryRules : TestWatcher() {
     var testClassPrefix = testEnvironmentProperties.testClassPrefix
-    var kranberry = "kranberry"
-
 
     override fun failed(e: Throwable?, description: Description?) {
+        failedTestCount++
         val methodName = description?.methodName
 
         setScreenshotsName("failed_$methodName")
         testResult("FAILED")
         takeScreenshot()
-        Log.testFailed(currentTestClassName, currentTestMethodName, e)
+        Log.testFailed(e)
 
     }
 
     override fun starting(description: Description?) {
+        device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
 
         currentTestClassName = description!!.className.removePrefix(testClassPrefix)
         currentTestMethodName = description.methodName
 
+        setScreenshotsName(currentTestMethodName)
+
         if (testClassName != currentTestClassName) {
             testClassName(currentTestClassName)
-            Log.alert("Starting tests of $currentTestClassName")
+            Log.startClass()
         }
-       Log.startTest(currentTestClassName, currentTestMethodName)
+        Log.startTest()
     }
 
     override fun succeeded(description: Description?) {
+        passedTestCount++
         testResult("PASSED")
         Log.testPassed(currentTestClassName, currentTestMethodName)
     }
 
     override fun finished(description: Description?) {
-        totalTestCount++
+        updateSummary()
         writeResult()
-        val passedRate = round((passedTestCount / totalTestCount)*100)
-        val failedRate = round((failedTestCount / totalTestCount)*100)
 
-        Log.info("\nExecution Summary:")
-        Log.info("EXECUTED: ${totalTestCount.toInt()}")
-        Log.success("PASSED: ${passedTestCount.toInt()} ($passedRate%)")
-        Log.error("FAILED: ${failedTestCount.toInt()} ($failedRate%)")
+        Log.testSummary()
     }
 
     private fun writeResult() {
-        if(!reportHasHeader){
+        if (!reportHasHeader) {
             File(getExecutionScreenshotPath() + "/results.csv")
-                .appendText("CLASS,TEST_METHOD,RESULT\n")
+                .appendText("TEST_CLASS,TEST_METHOD,RESULT\n")
             reportHasHeader = true
         }
         File(getExecutionScreenshotPath() + "/results.csv")
