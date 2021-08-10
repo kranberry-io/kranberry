ffa00b9844bc01f5515dc430eea4bf4ea93dd7ce
Ana Ludmila <kn.ludi@gmail.com>
2021-08-10 11:52:10 -0300

    #11 @knludi - Adding assert to test

--- src/androidTest/kotlin/io/kranberry/App.kt
+++ src/androidTest/kotlin/io/kranberry/App.kt
@@ -5,12 +5,15 @@ import io.kranberry.ScreenshotHandler.takeScreenshot
 import io.kranberry.environment.DeviceHandler
 import io.kranberry.environment.DeviceHandler.APP_PACKAGE
 import io.kranberry.ui.BaseUi
+import io.kranberry.ui.elementIsPresentByTextContains
+import junit.framework.TestCase.assertTrue
 
 open class App(device: UiDevice) : BaseUi(device) {
 
     open fun open(): App {
         DeviceHandler.start(APP_PACKAGE)
         takeScreenshot()
+        assertTrue(elementIsPresentByTextContains("Welcome to Chrome"))
         return this
     }
 }
