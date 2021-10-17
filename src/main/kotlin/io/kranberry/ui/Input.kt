e657f87ee298e7ee24510cfb91123fb005ace586
Ana Ludmila <kn.ludi@gmail.com>
2021-10-17 00:10:12 -0300

    #26 @knludi - Including Inputs handler class

---
+++ src/main/kotlin/io/kranberry/ui/Input.kt
@@ -0,0 +1,22 @@
+package io.kranberry.ui
+
+import io.kranberry.log.Log
+
+fun BaseUi.inputTextById(id: String, text: String): Boolean {
+    val input = findElementById(id)
+    input.text = text
+    return if (input.text == text) {
+        Log.message("The text '$text' was entered in the field 'id=$id'")
+        true
+    } else false
+}
+
+fun BaseUi.inputTextByIndex(index: Int, text: String): Boolean {
+    val input = findElementByIndex(index)
+    input.click()
+    input.text = text
+    return if (input.text == text) {
+        Log.message("The text '$text' was entered in the field 'id=$index'")
+        true
+    } else false
+}
