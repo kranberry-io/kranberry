441726aff4300b33e13405e3b25f65420cd9e116
Ana Ludmila <kn.ludi@gmail.com>
2021-10-17 00:08:49 -0300

    #26 @knludi - Including Asserts handler class

--- src/main/kotlin/io/kranberry/ui/Asserts.kt
+++ src/main/kotlin/io/kranberry/ui/Asserts.kt
@@ -1,17 +1,23 @@
 package io.kranberry.ui
 
 import io.kranberry.log.Log
+import io.kranberry.outputs.ScreenshotHandler.takeScreenshot
+import org.junit.Assert.fail
 
-fun BaseUi.inputTextById(id: String, text: String): Boolean {
-    val input = findElementById(id)
-    input.text = text
-    return if (input.text == text) {
-        Log.message("The text '$text' was entered in the field 'id=$id'")
-        true
-    } else false
+fun BaseUi.assertTrue(assumption: Boolean) {
+    when {
+        assumption -> {
+            Log.success("Successful validation!")
+            takeScreenshot()
+        }
+        else -> {
+            takeScreenshot()
+            fail("Validation unsuccessful!")
+        }
+    }
 }
 
-fun BaseUi.inputTextByIndex(index: Int, text: String): Boolean {
+fun BaseUi.assertFalse(index: Int, text: String): Boolean {
     val input = findElementByIndex(index)
     input.click()
     input.text = text
